dfc458e0789b4da73550c17665877f16c07e4b3c
Olga Denisyuk <denisykolga@yandex.ru>
2019-03-23 12:30:08 +0300

    feat: craet new function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var coins = { 
+        'H': 50,
+        'Q': 25,
+        'D': 10,
+        'N': 5,
+        'P': 1
+    };
+    var result = {};
+
+    for (var key in coins) {
+        var n = Math.floor(amount / coins[key]);
+        if (n > 0) { 
+            result[key] = n; 
+            amount -= n * coins[key]; 
+        }
+    }
+
+    return result;
 }
